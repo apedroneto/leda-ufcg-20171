ce169bbec34a17edc095bfd2ec400ed8c4e97306
rerissondaniel <rerissondaniel@gmail.com>
2017-05-24 17:09:09 -0300

    Roteiro 01 WIP

---
+++ R01-01-Rot-SimpleSorting-Simultaneous-Bubble-environment/src/main/java/sorting/simpleSorting/InsertionSort.java
@@ -0,0 +1,25 @@
+package sorting.simpleSorting;
+
+import sorting.AbstractSorting;
+
+/**
+ * As the insertion sort algorithm iterates over the array, it makes the
+ * assumption that the visited positions are already sorted in ascending order,
+ * which means it only needs to find the right position for the current element
+ * and insert it there.
+ */
+public class InsertionSort<T extends Comparable<T>> extends AbstractSorting<T> {
+
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			T aux = array[i];
+			int j = i - 1;
+			while(j >= 0 && array[j].compareTo(aux) > 0){
+				array[j + 1] = array[j];
+				j--;
+			}
+			array[j + 1] = aux;
+		}
+	}
+}
---
+++ R01-01-Rot-SimpleSorting-Simultaneous-Bubble-environment/src/main/java/sorting/variationsOfBubblesort/SimultaneousBubblesort.java
@@ -0,0 +1,32 @@
+package sorting.variationsOfBubblesort;
+
+import sorting.AbstractSorting;
+import util.Util;
+
+/**
+ * This algorithm applies two bubblesorts simultaneously. In a same iteration, a
+ * bubblesort pushes the greatest elements to the right and another bubblesort
+ * pushes the smallest elements to the left. At the end of the first iteration,
+ * the smallest element is in the first position (index 0) and the greatest
+ * element is the last position (index N-1). The next iteration does the same
+ * from index 1 to index N-2. And so on. The execution continues until the array
+ * is completely ordered.
+ */
+public class SimultaneousBubblesort<T extends Comparable<T>> extends
+		AbstractSorting<T> {
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			for (int j = i + 1; j <= rightIndex; j++) {
+				if (array[i].compareTo(array[j]) > 0) {
+					Util.swap(array, i, j);
+				}
+			}
+			for(int j = rightIndex - i; j >= leftIndex + i; j--){
+				if(array[i].compareTo(array[j]) > 0){
+					Util.swap(array, i, j);
+				}
+			}
+		}
+	}
+}
