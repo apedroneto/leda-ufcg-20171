bdedb52a1308ab64801311a1dd07aca965279a9a
rerissondaniel <rerissondaniel@gmail.com>
2017-06-19 15:18:25 -0300

    Double linked list implementada. Corrigir erros.

--- R06-01-Rot-Linked-list-abordagem-iterativa-environment/src/main/java/adt/linkedList/DoubleLinkedListImpl.java
+++ R06-01-Rot-Linked-list-abordagem-iterativa-environment/src/main/java/adt/linkedList/DoubleLinkedListImpl.java
@@ -4,23 +4,40 @@ public class DoubleLinkedListImpl<T> extends SingleLinkedListImpl<T> implements
 		DoubleLinkedList<T> {
 
 	protected DoubleLinkedListNode<T> last;
+	
+	public DoubleLinkedListImpl() {
+		this.last = new DoubleLinkedListNode<T>();
+		setHead(last);
+	}
 
 	@Override
 	public void insertFirst(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		DoubleLinkedListNode<T> newNode = new DoubleLinkedListNode<T>(element, getHead(), new DoubleLinkedListNode<T>());
+		setHead(newNode);
+		if(last.isNIL()){
+			setLast(newNode);
+			newNode.next = new DoubleLinkedListNode<T>();
+		}
 	}
 
 	@Override
 	public void removeFirst() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if(!getHead().isNIL()){
+			setHead(getHead().next);
+			getHead().previous = new DoubleLinkedListNode<T>();
+		}
+		//Caso não haja mais nenhum elemento na lista, devemos atualizar a cauda também.
+		if(getHead().next.isNIL()){
+			setLast(getHead());
+		}
 	}
 
 	@Override
 	public void removeLast() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if(!getLast().isNIL()){
+			setLast(getLast().previous);
+			getLast().next = new DoubleLinkedListNode<T>();
+		}
 	}
 
 	public DoubleLinkedListNode<T> getLast() {
@@ -31,4 +48,8 @@ public class DoubleLinkedListImpl<T> extends SingleLinkedListImpl<T> implements
 		this.last = last;
 	}
 
+	@Override
+	public DoubleLinkedListNode<T> getHead(){
+		return (DoubleLinkedListNode<T>)super.getHead();
+	}
 }
