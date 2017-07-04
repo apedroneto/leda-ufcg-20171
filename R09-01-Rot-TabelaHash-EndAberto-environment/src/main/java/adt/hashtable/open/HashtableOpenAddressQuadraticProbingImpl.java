4037a6f3131b092dc14aa061f0764da27bcfa7a7
Rerisson Daniel <rerissondaniel@gmail.com>
2017-07-03 21:54:58 -0300

    Nao vou comentar

--- R09-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressQuadraticProbingImpl.java
+++ R09-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressQuadraticProbingImpl.java
@@ -4,35 +4,74 @@ import adt.hashtable.hashfunction.HashFunctionClosedAddressMethod;
 import adt.hashtable.hashfunction.HashFunctionQuadraticProbing;
 
 public class HashtableOpenAddressQuadraticProbingImpl<T extends Storable>
-		extends AbstractHashtableOpenAddress<T> {
+        extends AbstractHashtableOpenAddress<T> {
 
-	public HashtableOpenAddressQuadraticProbingImpl(int size,
-			HashFunctionClosedAddressMethod method, int c1, int c2) {
-		super(size);
-		hashFunction = new HashFunctionQuadraticProbing<T>(size, method, c1, c2);
-		this.initiateInternalTable(size);
-	}
+    public HashtableOpenAddressQuadraticProbingImpl(int size,
+                                                    HashFunctionClosedAddressMethod method, int c1, int c2) {
+        super(size);
+        hashFunction = new HashFunctionQuadraticProbing<>(size, method, c1, c2);
+        this.initiateInternalTable(size);
+    }
 
-	@Override
-	public void insert(T element) {
+    @Override
+    public void insert(T element) {
+        if (isFull()) {
+            throw new HashtableOverflowException();
+        }
+        if (element != null && indexOf(element) == -1) {
+            boolean isInserted = false;
+            for (int probe = 0; probe < this.table.length && !isInserted; probe++) {
+                int hash = ((HashFunctionQuadraticProbing) getHashFunction()).hash(element, probe);
+                if (this.table[hash] == null || this.deletedElement.equals(this.table[hash])) {
+                    this.table[hash] = element;
+                    isInserted = true;
+                    COLLISIONS += probe;
+                }
+            }
 
-	}
+            if (isInserted) {
+                this.elements++;
+            }
+        }
+    }
 
-	@Override
-	public void remove(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    @Override
+    public void remove(T element) {
+        int idx = indexOf(element);
+        if (idx != -1) {
+            this.table[idx] = deletedElement;
+            this.elements--;
+        }
+    }
 
-	@Override
-	public T search(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    @Override
+    public T search(T element) {
+        T result = null;
+        int idx = indexOf(element);
+        if (idx != -1) {
+            result = (T) table[idx];
+        }
 
-	@Override
-	public int indexOf(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+        return result;
+    }
+
+    @Override
+    public int indexOf(T element) {
+        int result = -1;
+
+        if (element != null) {
+            int probe = 0;
+            int hash;
+            do {
+                hash = ((HashFunctionQuadraticProbing) getHashFunction()).hash(element, probe);
+                if (this.table[hash] != null && this.table[hash].equals(element)) {
+                    result = hash;
+                }
+                probe++;
+            } while (this.table[hash] != null && !this.table[hash].equals(element) && probe != this.table.length);
+        }
+
+        return result;
+    }
+
 }
